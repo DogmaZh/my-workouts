4f76385b179679c58ccbccdc59047c45adb5cbb9
DogmaZh <dogmevich@gmail.com>
2023-03-29 18:27:02 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/eslint-module",
+    "@pinia/nuxt",
+    "nuxt-icon",
+    "nuxt-typed-router",
+  ],
+});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <div class="mx-auto flex items-center justify-center">Homepage</div>
+    <Icon name="mdi:close" />
+    <nav>
+      <ol>
+        <li>
+          <nuxt-link to="/"></nuxt-link>
+        </li>
+      </ol>
+    </nav>
+  </div>
+</template>
